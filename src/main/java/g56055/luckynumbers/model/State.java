4cc768e69dec8f7d318dcb33adfa6a27f37a7d27
56055 <56055@etu.he2b.be>
2021-03-20 20:18:07 +0100

    "Project : State Enum class and Model Interface (section 4.2)"

---
+++ src/main/java/g56055/luckynumbers/model/State.java
@@ -0,0 +1,30 @@
+package g56055.luckynumbers.model;
+
+/**
+ *
+ * @author Duran Rehan g56055
+ *
+ */
+public enum State {
+
+    /**
+     * Initial state of the game
+     */
+    NOT_STARTED,
+    /**
+     * State of the tile selection
+     */
+    PICK_TILE,
+    /**
+     * Tile placement state
+     */
+    PLACE_TILE,
+    /**
+     * End of game state
+     */
+    GAME_OVER,
+    /**
+     * Game turn rotation
+     */
+    TURN_END
+}
